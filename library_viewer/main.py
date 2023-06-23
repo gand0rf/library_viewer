e0ef97db3fa8034966735fbb21347c4c51443c36
Gand0rf <gand0rf@protonmail.com>
2023-06-22 21:17:08 -0400

    Added proper formatting

--- library_viewer/main.py
+++ library_viewer/main.py
@@ -3,24 +3,44 @@ import sys
 import subprocess
 from Package_Checker import Package_Checker
 
-pkgs = pr.working_set
-pkg_list = sorted(["%s" % i.key for i in pkgs]+list(sys.stdlib_module_names))
+def get_package_list():
+	pkgs = pr.working_set
+	pkg_list = sorted(["%s" % i.key for i in pkgs]+list(sys.stdlib_module_names))
 
-for i in range(0, len(pkg_list)):
-    print(f'{i} {pkg_list[i]}')
+	count = 0
+	for p_id in range(0,len(pkg_list)):
+    		if '_' in pkg_list[p_id]:
+        		count += 1
+        
+	if count > 0:
+    		for id in range(0,count):
+        		pkg_list.pop(0)
 
-selection = int(input('\nEnter package number you want to look into: '))
-package = Package_Checker().pkge_check(pkg_list[selection])
+	for i in range(0, len(pkg_list)):
+    		print(f'{i} {pkg_list[i]}')
 
-file = open('package.py', 'w')
-file.writelines(f'import {package}\n')
-file.writelines(f'print(dir({package}))\n')
-file.close()
 
-pkg_run = subprocess.Popen(['python3', 'package.py'], stdout=subprocess.PIPE)
-pkg_sublist = pkg_run.communicate()[0].decode('utf-8')
-pkg_sublist = pkg_sublist.replace("['", "").replace("']", "").replace("\n", "").split("', '")
-print(f'\n{package}:')
-for sub in pkg_sublist:
-    if '__' not in sub and sub != 'main':
-        print('\t' + sub) 
+	selection = int(input('\nEnter package number you want to look into: '))
+	package = Package_Checker().pkge_check(pkg_list[selection])
+	
+	return package
+
+def write_package(package):
+	file = open('package.py', 'w')
+	file.writelines(f'import {package}\n')
+	file.writelines(f'print(dir({package}))\n')
+	file.close()
+
+def list_pkg_directory(package):
+	pkg_run = subprocess.Popen(['python3', 'package.py'], stdout=subprocess.PIPE)
+	pkg_sublist = pkg_run.communicate()[0].decode('utf-8')
+	pkg_sublist = pkg_sublist.replace("['", "").replace("']", "").replace("\n", "").split("', '")
+	print(f'\n{package}:')
+	for sub in pkg_sublist:
+    		if '__' not in sub and sub != 'main':
+        		print('\t' + sub) 
+
+if __name__ == "__main__":
+	package = get_package_list()
+	write_package(package)
+	list_pkg_directory(package)
